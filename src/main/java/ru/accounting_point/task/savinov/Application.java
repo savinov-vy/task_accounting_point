c1bf69f4dc2d269233892070a047721289218391
savinov85 <savinov_vu@mail.ru>
2020-10-03 08:21:39 +0800

    project created

---
+++ src/main/java/ru/accounting_point/task/savinov/Application.java
@@ -0,0 +1,13 @@
+package ru.accounting_point.task.savinov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
